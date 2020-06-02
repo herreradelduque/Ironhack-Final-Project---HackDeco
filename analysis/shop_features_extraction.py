f4a62a056aa50e65a23257c4e008fdfed45178b8
Víctor Galán <herreradelduque@gmail.com>
2020-06-03 00:40:59 +0200

    cleaned shop_feat_extreactor, user_feat_extractor and visual_recom

--- analysis/shop_features_extraction.py
+++ analysis/shop_features_extraction.py
@@ -1,12 +1,10 @@
 # imports
-
 from keras.applications import vgg16
 from keras.preprocessing.image import load_img,img_to_array
 from keras.models import Model
 from keras.applications.imagenet_utils import preprocess_input
 import os
 import numpy as np
-
 
 def features_extraction():
     # parameters setup
@@ -17,57 +15,29 @@ def features_extraction():
         for j, product in enumerate(product_lst):
             print('Getting path...')
             imgs_path = '../data/' + 'img_' + shop.lower() + '_' + product + '/'
-
             imgs_model_width, imgs_model_height = 224, 224
-
             ##  1. load the VGG pre-trained model from Keras
-
             # load the model
             vgg_model = vgg16.VGG16(weights='imagenet')
-
             # remove the last layers in order to get features instead of predictions
             feat_extractor = Model(inputs=vgg_model.input, outputs=vgg_model.get_layer("fc2").output)
-
             # 2. get the images paths
-
             files = [imgs_path + x for x in os.listdir(imgs_path) if "jpg" in x]
-
             print("number of images:",len(files))
-
-
             # 4. feed all the images into the CNN
-
             # load all the images and prepare them for feeding into the CNN
-
             importedImages = []
-
             for f in files:
                 filename = f
                 original = load_img(filename, target_size=(imgs_model_width, imgs_model_height))
                 numpy_image = img_to_array(original)
                 image_batch = np.expand_dims(numpy_image, axis=0)
-
                 importedImages.append(image_batch)
-
             images = np.vstack(importedImages)
-
             processed_imgs = preprocess_input(images.copy())
-
             # extract the images features
-
             imgs_features = feat_extractor.predict(processed_imgs)
-
             print("features successfully extracted!")
-
-
-            #print('---------------')
-
-            #print(imgs_features)
-
-
-            #imgs_features = np.append(files,imgs_features, axis=1)
-            #print("filenames appended to images features!")
-
             np.save(f'../data/features_extraction_products/img_features_{shop_lst[i]}_{product_lst[j]}', imgs_features)
             print(f"img_features_user saved at: '../data/features_extraction_products/img_features_{shop_lst[i]}_{product_lst[j]}'")
 features_extraction()
--- analysis/visual_recommendations.py
+++ analysis/visual_recommendations.py
@@ -1,15 +1,12 @@
 # imports
-
 import os
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-
 def cos_simil(shop,product,img_features_user):
     shop_list = ['Ikea']
     products_list = ['Sillas', 'Sofás']
-
     if shop in shop_list:
         if product in products_list:
             imgs_path = f'./data/img_{shop.lower()}_{product}/'
@@ -17,7 +14,6 @@ def cos_simil(shop,product,img_features_user):
             files = [imgs_path_arrange + x for x in os.listdir(imgs_path) if "jpg" in x]
         # 5. compute cosine similarities
             imgs_features = np.load(f'./data/features_extraction_products/img_features_{shop}_{product}.npy')
-            img_features_user #= np.load('./data/features_extraction_user/img_features_user.npy')
             # compute cosine similarities between images
             cos_similarities = cosine_similarity(imgs_features,img_features_user)
             cos_similarities = pd.DataFrame(cos_similarities)
@@ -25,8 +21,6 @@ def cos_simil(shop,product,img_features_user):
             furni_df = pd.read_csv(f'./data/{product}.csv')
             cos_similarities_merged_df = cos_similarities.merge(furni_df, left_on='Local_Path_cos', right_on='Local_Path')
             cos_similarities_merged_df = cos_similarities_merged_df.sort_values(by=0, ascending=False)
-        # Save as csv:
-            #cos_similarities_merged_df.to_csv(f'../data/cos_similarities_{shop.lower()}_{product}.csv', index=False)
             # Save json:
             cos_similarities_merged_df.to_json(f'./data/cos_similarities_{shop.lower()}_{product}.json', orient='records')
         else:
